12b2216a46a7262853ad6b67cc3048d704b03b79
Matin <mtynmnswry092@gmail.com>
2024-04-26 20:31:49 +0330

    init

---
+++ CHANGELOG.md
@@ -0,0 +1,4 @@
+## 1.0.0
+
+-   Forked The Pedometer Plugin
+-   added daily emitting for ios(android still is under development)
---
+++ example/ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,21 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#import "GeneratedPluginRegistrant.h"
+
+#if __has_include(<pedometer/PedometerPlugin.h>)
+#import <pedometer/PedometerPlugin.h>
+#else
+@import pedometer;
+#endif
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [PedometerPlugin registerWithRegistrar:[registry registrarForPlugin:@"PedometerPlugin"]];
+}
+
+@end
